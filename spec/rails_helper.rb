6fd02f028d8f18bb87e14cba9d1fa008927588e4
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-10-21 02:33:34 -0300

    add: transferences tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
+  config.include ActiveSupport::Testing::TimeHelpers
+
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
--- spec/services/transference_spec.rb
+++ spec/services/transference_spec.rb
@@ -29,6 +29,101 @@ RSpec.describe Transference do
         )
         expect(transference.call?).to be_falsey
       end
+
+      context "when is not working time" do
+        let(:recipient) { create(:bank_account) }
+        let(:sender) { create(:bank_account) }
+        let(:value) { FFaker::Random.rand(100.0..500.0).round(2) }
+        let(:value_bigger_thousand) { FFaker::Random.rand(1000.0..1500.0).round(2) }
+
+        before do
+          travel_to Time.zone.parse("2020-10-20 23:33:43")
+          @transference = Transference.new(
+            value: value,
+            recipient: recipient,
+            sender: sender,
+          )
+
+          @transference_bigger_value = Transference.new(
+            value: value_bigger_thousand,
+            recipient: recipient,
+            sender: sender,
+          )
+        end
+
+        it "does create transference out of working time" do
+          expect {
+            @transference.call?
+          }.to change(Transaction, :count).by(1)
+        end
+
+        it "verifies if .call returns true" do
+          expect(@transference.call?).to be_truthy
+        end
+
+        it "does create transference with value over than a thousand" do
+          new_balance_sender = sender.balance - value_bigger_thousand - 10 - 7
+          new_balance_recipient = recipient.balance + value_bigger_thousand
+          @transference_bigger_value.call?
+          expect(sender.reload.balance).to eq(new_balance_sender)
+          expect(recipient.reload.balance).to eq(new_balance_recipient)
+        end
+
+        it "does create transference with value lower than a thousand" do
+          new_balance_sender = sender.balance - value - 7
+          new_balance_recipient = recipient.balance + value
+          @transference.call?
+          expect(sender.reload.balance).to eq(new_balance_sender)
+          expect(recipient.reload.balance).to eq(new_balance_recipient)
+        end
+      end
+      context "when is in working time" do
+        let(:recipient) { create(:bank_account) }
+        let(:sender) { create(:bank_account) }
+        let(:value) { FFaker::Random.rand(100.0..500.0).round(2) }
+        let(:value_bigger_thousand) { FFaker::Random.rand(1000.0..1500.0).round(2) }
+
+        before do
+          travel_to Time.zone.parse("2020-10-20 12:32:42")
+          @transference = Transference.new(
+            value: value,
+            recipient: recipient,
+            sender: sender,
+          )
+
+          @transference_bigger_value = Transference.new(
+            value: value_bigger_thousand,
+            recipient: recipient,
+            sender: sender,
+          )
+        end
+
+        it "does create transference in working time" do
+          expect {
+            @transference.call?
+          }.to change(Transaction, :count).by(1)
+        end
+
+        it "verifies if .call returns true" do
+          expect(@transference.call?).to be_truthy
+        end
+
+        it "does create transference with value over than a thousand" do
+          new_balance_sender = sender.balance - value_bigger_thousand - 10 - 5
+          new_balance_recipient = recipient.balance + value_bigger_thousand
+          @transference_bigger_value.call?
+          expect(sender.reload.balance).to eq(new_balance_sender)
+          expect(recipient.reload.balance).to eq(new_balance_recipient)
+        end
+
+        it "does create transference with value lower than a thousand" do
+          new_balance_sender = sender.balance - value - 5
+          new_balance_recipient = recipient.balance + value
+          @transference.call?
+          expect(sender.reload.balance).to eq(new_balance_sender)
+          expect(recipient.reload.balance).to eq(new_balance_recipient)
+        end
+      end
     end
   end
 end
