9fbfd07f95784dbcbb2fd75c7e53001817fa3d6a
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-10-21 02:32:57 -0300

    add: home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :deposits, only: [:new, :create]
   resources :withdrawals, only: [:new, :create]
   resources :transferences, only: [:new, :create]
 
+  root to: "home#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
