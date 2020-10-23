a3514ddcf6cb6aa4bbffe2c301218f8089139962
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-10-22 23:14:44 -0300

    add:autentication for withdraw and tranference

--- app/controllers/transferences_controller.rb
+++ app/controllers/transferences_controller.rb
@@ -7,16 +7,22 @@ class TransferencesController < ApplicationController
   end
 
   def create
-    transference_service = Transference.new(
-      value: transference_params[:value],
-      sender: current_user.bank_account,
-      recipient: @recipient,
-    )
-    respond_to do |format|
-      if transference_service.call?
-        format.html { redirect_to root_path, notice: "Transference was successfully executed." }
-      else
-        format.html { render :new, notice: "There was an error with your transference." }
+    if current_user.valid_password?(transference_params[:password])
+      transference_service = Transference.new(
+        value: transference_params[:value],
+        sender: current_user.bank_account,
+        recipient: @recipient,
+      )
+      respond_to do |format|
+        if transference_service.call?
+          format.html { redirect_to root_path, notice: "Transference was successfully executed." }
+        else
+          format.html { redirect_to new_transference_path, notice: "There was an error with your transference." }
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to new_transference_path, notice: "Incorrect password" }
       end
     end
   end
@@ -27,12 +33,12 @@ class TransferencesController < ApplicationController
     params.require(:transaction)
       .permit(
         :value,
-        :recipient
+        :recipient,
+        :password
       )
   end
 
   def set_recipient
     @recipient = BankAccountQuery.find_account_by_number(transference_params[:recipient])
-    binding.pry
   end
 end
