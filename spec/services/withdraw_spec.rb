aedfb43e7152fd01f909540f887306a7024c2f87
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-10-23 17:32:45 -0300

    refactor: withdraw test file

--- spec/services/withdraw_spec.rb
+++ spec/services/withdraw_spec.rb
@@ -6,27 +6,36 @@ RSpec.describe Withdraw do
       let(:bank_account) { create(:bank_account) }
       let(:value) { FFaker::Random.rand(100.0..500.0).round(2) }
 
+      before do
+        @withdraw = Withdraw.new(value: value, recipient: bank_account)
+      end
+
       it "does create Withdraw with valid params" do
-        withdraw = Withdraw.new(value: value, recipient: bank_account)
-        expect(withdraw.call?).to be_truthy
+        expect(@withdraw.call?).to be_truthy
       end
     end
+
     context "when call is false" do
       let(:bank_account) { create(:bank_account) }
       let(:value) { FFaker::Random.rand(10000.0..50000.0).round(2) }
 
+      before do
+        @withdraw = Withdraw.new(value: value, recipient: bank_account)
+      end
+
       it "does not create Withdraw with invalid params" do
-        withdraw = Withdraw.new(value: value, recipient: bank_account)
-        expect(withdraw.call?).to be_falsey
+        expect(@withdraw.call?).to be_falsey
       end
     end
 
     context "when withdraw is made " do
       let(:bank_account) { create(:bank_account) }
       let(:value) { FFaker::Random.rand(100.0..500.0).round(2) }
+
       before do
         @withdraw = Withdraw.new(value: value, recipient: bank_account)
       end
+
       it "does create withdraw" do
         new_balance_recipient = bank_account.balance - value
         @withdraw.call?
